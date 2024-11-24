67d39692f4ee4102f961392ed8b1bdbd118be3ea
Aiyat29 <146120678+Aiyat29@users.noreply.github.com>
2024-11-24 23:55:26 +0600

    models.py

---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    products = relationship("Product", back_populates="category")
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float)
+    quantity = Column(Integer)
+    available = Column(Boolean, default=True)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+
+    category = relationship("Category", back_populates="products")
